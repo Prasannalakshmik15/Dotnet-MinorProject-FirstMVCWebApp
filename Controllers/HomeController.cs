a46fafc4fa312b8e0cace1a2e27ce1ac050c7168
prasanna <prasannalakshmik15@gmail.com>
2025-01-27 17:42:12 +0530

    FirstMVCWebApp

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using FirstMVCWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            student.Name = "Prasanna";
+            student.City = "Kadapa";
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstMVCWebApp.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string City { get; set; }
+
+    }
+}
